511765646dd74463f50241a1f02147930bb25198
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-07-03 19:16:02 -0400

    Update cdagcubesengine.js

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -260,7 +260,6 @@
 
 
 									//export 
-var window.scene;
 
 									main();
 									//https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
@@ -276,7 +275,8 @@ var window.scene;
 						    );
 						    return;
 						  }
-							scene = new CdagScene(gl);
+							window.scene = new CdagScene(gl);
+							let scene = window.scene;
 						  // Set clear color to black, fully opaque
 						  gl.clearColor(0.0, 0.0, 0.0, 1.0);
 						  // Clear the color buffer with specified clear color
