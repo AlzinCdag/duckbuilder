8ffa572e1c30d8e79b6ecd951138fc164c84a2c6
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-07-02 22:12:44 -0400

    Added object variable "scene" to the code outside of the main() method so other javascript files can access it

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -259,6 +259,8 @@
 						
 
 
+									var scene;
+
 									main();
 									//https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
 						function main() {
@@ -273,7 +275,7 @@
 						    );
 						    return;
 						  }
-							const sceneObject = new CdagScene(gl);
+							scene = new CdagScene(gl);
 						  // Set clear color to black, fully opaque
 						  gl.clearColor(0.0, 0.0, 0.0, 1.0);
 						  // Clear the color buffer with specified clear color
