b9a3c01badd19b008a66cf7e947510cdbec80733
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-07-08 21:33:30 -0400

    Update cdagcubesengine.js

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -346,6 +346,8 @@
 							
 							for (let i = 0; i<this.listOfAnimationInformation.length; i++) {
 								const anim = this.listOfAnimationInformation[i];
+								anim[1].fillStyle = 'rgba(0,0,0,0)';
+								anim[1].fillRect(0,0,anim[3],anim[4]);
 								anim[1].drawImage(anim[2],0 + anim[3]*anim[6],0,anim[3],anim[4],0,0,anim[3],anim[4]);
 								anim[6]++;
 								if (anim[6] >= anim[5]) {anim[6] = 0;}
