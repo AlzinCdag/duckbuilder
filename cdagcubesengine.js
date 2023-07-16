e60950cba8b06cb52ffecae6dd81570809710fc4
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-07-16 17:41:46 -0400

    Update cdagcubesengine.js

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -68,15 +68,6 @@
 						    void main(void) {
 						      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
 						      vTextureCoord = aTextureCoord;
-
-									highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-      						highp vec3 directionalLightColor = vec3(1, 1, 1);
-      						highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
-
-      						highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-
-      						highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
-      						vLighting = ambientLight + (directionalLightColor * directional);
 						    }
 						  `;
 
@@ -185,6 +176,18 @@
 						    }
 						  `;
 
+const fsSourceFlat = `
+						    varying highp vec2 vTextureCoord;
+						    varying highp vec3 vLighting;
+						    uniform sampler2D uSampler;
+
+						    void main(void) {
+						      highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
+									gl_FragColor = vec4(texelColor.rgb, texelColor.a);
+						    }
+						  `;
+
+
 							const pointsInGridArray = 792;
 
 						let sceneRotX = 0;
@@ -228,7 +231,7 @@
 								this.listOfTypesOfFlatThings = [];
 
 								this.shaderProgramTex = initShaderProgram(this.gl, vsSourceTex, fsSourceTex);
-								this.shaderProgramFlat = initShaderProgram(this.gl, vsSourceFlat, fsSourceTex);
+								this.shaderProgramFlat = initShaderProgram(this.gl, vsSourceFlat, fsSourceFlat);
 								this.programInfoTex = {
 						  program: this.shaderProgramTex,
 						  attribLocations: {
