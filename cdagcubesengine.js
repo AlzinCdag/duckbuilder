b74e4269d44e9b63108f0daff011ca77b824e10a
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-07-27 21:51:35 -0400

    Update cdagcubesengine.js

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -1370,7 +1370,7 @@ function drawFlatObject(gl, programInfo, buffers, rotX,rotY,rotZ, texture, proje
 						//let finalPositionOrientation = multiplyMatrixByVector4d(multiplyMatrixByVector4d(multiplyMatrixByVector4d([xShift,yShift,zShift,0],createRotationMatrix(rotX,rotY,rotZ)),createTransformationMatrix(0,0,-20)),invertMatrix(createRotationMatrix(rotX,rotY,rotZ)));
 						let finalPositionOrientation = multiplyMatrixByVector4d([xShift,yShift,zShift,0],createRotationMatrix(rotX,rotY,rotZ));
 
-						document.getElementById("versionNumber").innerHTML = "<p> Z:"+finalPositionOrientation[2] + "</p><p> Y: " + finalPositionOrientation[1]+"</p><p> X: "+finalPositionOrientation[0]+"</p>";
+						document.getElementById("versionNumber").innerHTML = createRotationMatrix(rotX,rotY,rotZ);
 						gl.uniform3f(programInfo.uniformLocations.centerLocation,finalPositionOrientation[0],finalPositionOrientation[1],finalPositionOrientation[2]);
 
 						//http://www.opengl-tutorial.org/intermediate-tutorials/billboards-particles/billboards/
