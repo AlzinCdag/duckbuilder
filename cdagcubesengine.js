37b81aa81e7b0c8958544f144e049a506a76d5cf
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-07-27 21:48:40 -0400

    Update cdagcubesengine.js

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -1736,10 +1736,10 @@ function setNormalAttribute(gl, buffers, programInfo) {
 
 							 */
 
-							return [[matrix[0][0]*vector[0][0]+matrix[0][1]*vector[1][0]+matrix[0][2]*vector[2][0]+matrix[0][3]*vector[3][0]],
-							        [matrix[1][0]*vector[0][0]+matrix[1][1]*vector[1][0]+matrix[1][2]*vector[2][0]+matrix[1][3]*vector[3][0]],
-						          [matrix[2][0]*vector[0][0]+matrix[2][1]*vector[1][0]+matrix[2][2]*vector[2][0]+matrix[2][3]*vector[3][0]],
-										  [matrix[3][0]*vector[0][0]+matrix[3][1]*vector[1][0]+matrix[3][2]*vector[2][0]+matrix[3][3]*vector[3][0]]]
+							return [matrix[0][0]*vector[0][0]+matrix[0][1]*vector[1][0]+matrix[0][2]*vector[2][0]+matrix[0][3]*vector[3][0],
+							        matrix[1][0]*vector[0][0]+matrix[1][1]*vector[1][0]+matrix[1][2]*vector[2][0]+matrix[1][3]*vector[3][0],
+						          matrix[2][0]*vector[0][0]+matrix[2][1]*vector[1][0]+matrix[2][2]*vector[2][0]+matrix[2][3]*vector[3][0],
+										  matrix[3][0]*vector[0][0]+matrix[3][1]*vector[1][0]+matrix[3][2]*vector[2][0]+matrix[3][3]*vector[3][0]];
 						}
 
 						function multiplyMatrices4d(m1,m2) {
