8c6dba61f2bd80acae6c2cf712395a93e94454e5
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-07-31 22:17:31 -0400

    Update cdagcubesengine.js

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -583,10 +583,10 @@ var counterNumber = 0;
 							//scene.addCubeToSceneSize("missileCube","missile1",-7,5,9,0.7);
 							//scene.addCubeToSceneSize("missileCube","missile2",-6,5,9,0.5);
 							//scene.addCubeToSceneSize("missileCube","missile3",-5,5,9,0.3);
-							scene.addFlatThingToSceneSize("BirdTestCube","bird1",2,2,1,1);
-							scene.addFlatThingToSceneSize("BirdTestCube","bird2",2,2,2,1);
-							scene.addFlatThingToSceneSize("BirdTestCube","bird3",2,2,3,1);
-							scene.addFlatThingToSceneSize("BirdTestCube","bird4",2,2,0,1);
+							scene.addFlatThingToSceneSize("BirdTestCube","bird1",7,9,9,1);
+							scene.addFlatThingToSceneSize("BirdTestCube","bird2",5,9,9,1);
+							scene.addFlatThingToSceneSize("BirdTestCube","bird3",3,9,9,1);
+							scene.addFlatThingToSceneSize("BirdTestCube","bird4",-1,9,9,1);
 						//scene.removeCubeFromScene("firstCube");
 
 						// Draw the scene repeatedly
