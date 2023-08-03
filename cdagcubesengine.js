bdf9bdcd3fdfd317eef48ba8943f7577d3311167
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-08-02 21:15:32 -0400

    Update cdagcubesengine.js

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -1358,7 +1358,7 @@ function drawFlatObject(gl, programInfo, buffers, rotX,rotY,rotZ, texture, proje
 						  gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
 						  //gl.clearDepth(1.0); // Clear everything
 						  gl.enable(gl.DEPTH_TEST); // Enable depth testing
-						  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+						  gl.depthFunc(gl.GEQUAL); // Near things obscure far things
 
 	let xShift = (xShifta - 0.5)/2;
 	let yShift = (yShifta- 0.5)/2;
