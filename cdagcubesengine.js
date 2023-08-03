dceaa73c20aa349ede18204fc628dee6a2512097
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-08-02 20:38:04 -0400

    Update cdagcubesengine.js

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -1458,8 +1458,8 @@ function drawGrid(gl, programInfo, buffers, rotX,rotY,rotZ, texture,projectionSc
 
 	gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
 	//gl.clearDepth(1.0); // Clear everything
-	gl.enable(gl.DEPTH_TEST); // Enable depth testing
-  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+	//gl.enable(gl.DEPTH_TEST); // Enable depth testing
+  //gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 	//gl.enable(gl.CULL_FACE);
 	//gl.cullFace(gl.FRONT);
 
