910fe2ae4a2aa682a5468c108b063be5f9b63223
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2023-08-05 20:21:09 -0400

    Update cdagcubesengine.js

--- cdagcubesengine.js
+++ cdagcubesengine.js
@@ -1389,12 +1389,12 @@ function drawFlatObject(gl, programInfo, buffers, rotX,rotY,rotZ, texture, proje
 						  gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
 						  //gl.clearDepth(1.0); // Clear everything
 						  gl.enable(gl.DEPTH_TEST); // Enable depth testing
-						  gl.depthFunc(gl.GEQUAL); // Near things obscure far things
+						  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
 	//inspiration from https://www.chinedufn.com/webgl-particle-effect-billboard-tutorial/
 	
-	gl.enable(gl.BLEND);
-	gl.blendFunc(gl.ONE,gl.ZERO);
+	//gl.enable(gl.BLEND);
+	//gl.blendFunc(gl.ONE,gl.ZERO);
 
 	let xShift = (xShifta - 0.5)/2;
 	let yShift = (yShifta- 0.5)/2;
