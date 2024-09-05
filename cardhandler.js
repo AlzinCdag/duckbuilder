147020f1f2d6e3b20a0b22ea84b21f68ebcbd6d9
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2024-09-04 20:21:57 -0400

    Update cardhandler.js

--- cardhandler.js
+++ cardhandler.js
@@ -8,15 +8,19 @@ class Card {
   onActivation() {}
   onDraw() {}
   onEndTurnInHand() {}
-  printCard() {}
+  printCard(div) {
+    div.getElementsByClassName("cardTitle")[0].innerHTML = title;
+    div.getElementsByClassName("cardInfo")[0].innerHTML = description;
+    div.getElementsByClassName("topButton")[0].onclick = this.onActivation();
+    
+  }
 }
 
 class BasicShot extends Card {
-  title = "Basic Shot";
-  description= "+ 7-shot";
+  super.title = "Basic Shot";
+  super.description= "+ 7-shot";
   onActivation() {
-    window.currentShip = new Ship();
-    
+    window.shipHandler.createBasicShot();
   }
   
 }
@@ -25,12 +29,52 @@ class CardHandler {
   constructor() {
    let b = new BasicShot();
     let c = new BasicShot();
+    let d = new BasicShot();
+    let e = new BasicShot();
+    let f = new BasicShot();
     
-    this.fullDeck = { b,c
+    this.fullDeck = { b,c,d,e,f
       
     }
+    this.drawPile = this.fullDeck;
+    this.discardPile = new Array();
+    this.currentHand = new Array();
+    
   }
   addCardToDeck(card) {
-    this.
+    this.fullDeck.push(card);
   }
+  drawFreshHand() {
+    for (let i = 0; i<5; i++) {
+    this.drawSingleCard();}
+    
+  }
+  drawSingleCard() {
+    if (this.currentHand.length < 10)
+    {
+      if(this.drawPile.length === 0) {
+        this.shuffleDiscardPileIntoDrawPile();
+      }
+    this.currentHand.push(this.drawPile.pop());
+    }
+  }
+
+  discardHand() {
+    while (this.currentHand.length != 0) {
+      this.discardPile.push(this.currentHand.pop())
+    }
+  }
+
+  discardSingleCard(index) {
+    this.discardPile.push(this.currentHand.splice(index,1));
+  }
+
+  shuffleDiscardPileIntoDrawPile() {
+    while (this.discardPile.length != 0) {
+      this.drawPile.push(this.discardPile.splice(Math.floor(Math.random()*this.discardPile.length),1));
+    }
+  }
+  
+
+
 }
