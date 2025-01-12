d8f898d3e133fff1837cab441481bf76bfa449ef
AlzinCdag <46981043+AlzinCdag@users.noreply.github.com>
2025-01-11 22:43:31 -0500

    Update shipHandler.js
    See if the matryoshka compiles. (can't hit it yet)

--- shipHandler.js
+++ shipHandler.js
@@ -39,6 +39,39 @@ class Ship {
     }
       
   }
+
+ placeDownInRandomUnoccupiedSpace() {
+  let xRand = 0;
+  let yRand = 0;
+  let zRand = 0;
+  let successful = false;
+   for (let i = 0; i<1000; i++;) {
+    xRand = Math.floor(Math.random()*10)-4;
+    yRand = Math.floor(Math.random()*10)-4;
+    zRand = Math.floor(Math.random()*10)-4;
+    if (!this.checkIfPlacementConflictsWithOtherShips(xRand,yRand,zRand)) {
+     placeDown(xRand,yRand,zRand);
+     successful = true;
+     break;
+    }
+   }
+if !(successful){
+ for (let i =-4; i<6; i++) {
+  for (let j =-4; j<6; j++) {
+   for (let k =-4; k<6; k++) {
+    if (!this.checkIfPlacementConflictsWithOtherShips(i,j,k)) {
+     placeDown(i,j,k);
+     successful = true;
+     break;
+    }
+     }//k
+   if (successful){break;}
+  }//j
+  if (successful){break;}
+ }//i
+}
+ return successful; 
+ }
   checkIfPlacementWithinBounds(xCoord,yCoord,zCoord) {
     let cubeLen = this.cubeShape.length;
     let isWithinBounds = true;
@@ -49,6 +82,37 @@ class Ship {
       }
     return isWithinBounds;
   }
+
+ checkIfPlacementConflictsWithOtherShips(xCoord,yCoord,zCoord) {
+  let cubeLen = this.cubeShape.length;
+  let placementConflicts = false;
+  if (!this.checkIfPlacementWithinBounds(xCoord,yCoord,zCoord)) {placementConflicts=true; return placementConflicts;}
+   for (let i=0; i<window.shipHandler.listOfShips1; i++) {
+    if (window.shipHandler.listOfShips1[i]) {
+     for (let j = 0; j< cubeLen; j++) {
+     if (window.shipHandler.listOfShips1[i].checkIfHit(xCoord+this.cubeShape[j][0],yCoord+this.cubeShape[j][1],zCoord+this.cubeShape[j][2]))
+     {placementConflicts=true;}
+    } //for cubes in ship
+    }//if exists
+   }//for ships in list 1
+     for (let i=0; i<window.shipHandler.listOfShips2; i++) {
+    if (window.shipHandler.listOfShips2[i]) {
+     for (let j = 0; j< cubeLen; j++) {
+     if (window.shipHandler.listOfShips2[i].checkIfHit(xCoord+this.cubeShape[j][0],yCoord+this.cubeShape[j][1],zCoord+this.cubeShape[j][2]))
+     {placementConflicts=true;}
+    } //for cubes in ship
+    }//if exists
+   }//for ships in list 2
+     for (let i=0; i<window.shipHandler.listOfNonShips; i++) {
+    if (window.shipHandler.listOfNonShips[i]) {
+     for (let j = 0; j< cubeLen; j++) {
+     if (window.shipHandler.listOfNonShips[i].checkIfHit(xCoord+this.cubeShape[j][0],yCoord+this.cubeShape[j][1],zCoord+this.cubeShape[j][2]))
+     {placementConflicts=true;}
+    } //for cubes in ship
+    }//if exists
+   }//for ships in list of non ships
+  
+ }
   getID() {return this.id;}
 
   // return the id of the hit cube, null otherwise
@@ -153,7 +217,12 @@ class ShipHandler {
   basicShotNumber = basicShotNumber+1;
   window.shipCurrentlyBeingPlaced = b;
   b.placeDown(window.cursorX,window.cursorY,window.cursorZ);
-  
+  window.shipHandler.listOfNonShips.push(b);
+  return b;
+ }
+  createMatryoshka() {
+  let b = new BasicShotProjectile("Matryoshka"+basicShotNumber);
+  basicShotNumber = basicShotNumber+1;
   return b;
  }
 }//shiphandler
@@ -162,10 +231,57 @@ class BasicShotProjectile extends Ship{
   constructor(name)
  {
   super("yellowPattern.png","basicShot",[[0,0,0],[0,0,1],[1,0,0],[0,1,0],[0,0,-1],[-1,0,0],[0,-1,0]],name);
+  
  }
  absoluteMove(xShift,yShift,zShift) {
    super.absoluteMoveFree(xShift,yShift,zShift);
  }
 }
 
+class MatryoshkaShip extends Ship{
+ constructor(name,side) {
+
+  if (name.indexOf("Small") !== -1) {
+  super("Fabergé_egg_Rome_08.JPG","Matryoshka",[[0,0,0],[0,1,0]],name);}
+  else if (name.indexOf("Medium") !== -1) {
+  super("Fabergé_egg_Rome_08.JPG","Matryoshka",[[0,0,0],[0,1,0]],name);}
+ else (name.indexOf("Large") !== -1) {
+  super("Fabergé_egg_Rome_08.JPG","Matryoshka",[[0,0,0],[0,1,0],[0,-1,0]],name);}
+  this.placeDownInRandomUnoccupiedSpace();
+  this.alreadyHit = false;
+    this.side = side;
+  if (side == 1) {
+   window.shipHandler.listOfShips1.push(this);
+  }
+  else {window.shipHandler.listOfShips2.push(this);}
+ }
+
+onBeingHit() {
+ super.onBeingHit();
+ let newName = name;
+if (!this.alreadyHit) {
+ switch (cubeShape.length) {
+  case 3:
+   newName.concat("Medium");
+   break;
+  case 2:
+   newName.concat("Small");
+   break;
+  case 1:
+   break;
+  default:
+   break;
+ }
+
+ if (newName !== name) {
+  let newShip = new MatryoshkaShip(newName,side);
+ }
+}
+ 
+ this.alreadyHit = true;
+ 
+}
+}
+
 window.shipHandler = new ShipHandler();
+window.shipHandler.createMatryoshka();
